e600276a2a8e34b4f076cc21fe7eee3a27cbe9fb
2883857@student.edu.az <2883857@student.edu.az>
2023-12-13 20:51:48 +0400

    functional version only works

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,121 +4,159 @@ const loseId = document.querySelector("#lose")
 const guessId = document.querySelector("#guess")
 const letterIsId = document.querySelector("#letterIs")
 
+let geusss = 9;
+let wins = 0;
+let loses = 0;
+let arrLetters = [];
+let randomLetter = "s";
 
-class ThePsychicGame {
-    win = 0;
-    lose = 0;
-    guess = 9;
-    letter = 0;
+
+function randomLetterFunc() {
+    let random = Math.floor(Math.random() * 26);
+    let arrAlphabet = "abcdefghijklmnopqrstuvwxy".split("");
+    return arrAlphabet[random];
+}
+
+function restartGame() {
+    geusss = 9;
+    guess.innerHTML = 9;
     arrLetters = [];
-    randomLetter = "s";
-
-    randomLetterFunc() {
-        let random = Math.floor(Math.random() * 26);
-        let arrAlphabet = "abcdefghijklmnopqrstuvwxy".split("");
-        return arrAlphabet[random];
-    };
-
-    restartGame() {
-        this.guess = 9;
-        guessId.innerHTML = 9;
-        this.arrLetters = [];
-        this.randomLetter = this.randomLetterFunc();
-    };
-
-
-    startGame() {
-
-        addEventListener("keypress", function(e){
-            if (e.key.match(/[a-z]/i)) {
-                if (this.guess !== 0) {
-                    if (e.key !== this.randomLetter) {
-                        this.arrLetters.push(e.key)
-                        this.guess--;
-                        guessId.innerHTML = this.guess;
-                        letterId.innerHTML = this.arrLetters.map((el) => {
-                            return el;
-                        }).join(", ");
-                    } else {
-                        this.win++;
-                        winId.innerHTML = this.win;
-                        letterIsId.innerHTML = this.randomLetter;
-                        guessId.innerHTML = 9;
-                        this.restartGame();
-                    }
-                    console.log(this.randomLetter);
-                } else {
-                    this.lose++;
-                    loseId.innerHTML = this.lose;
-                    this.restartGame();
-                }
-
-            }
-        })
-
-    };
-
+    randomLetter = randomLetterFunc();
 }
 
 
+function startGame() {
+
+    addEventListener("keypress", e => {
+        if (e.key.match(/[a-z]/i)) {
+            if (geusss !== 0) {
+                if (e.key !== randomLetter) {
+                    arrLetters.push(e.key)
+                    geusss--;
+                    guess.innerHTML = geusss;
+                    letter.innerHTML = arrLetters.map((el) => {
+                        return el;
+                    }).join(", ");
+                } else {
+                    wins++;
+                    win.innerHTML = wins;
+                    letterIs.innerHTML = randomLetter;
+                    guess.innerHTML = 9;
+                    restartGame();
+                }
+                console.log(randomLetter);
+            } else {
+                loses++;
+                lose.innerHTML = loses;
+                restartGame();
+            }
+
+        }
+    })
+
+};
+
+startGame();
 
 
-const ThePsychicGameObj = new ThePsychicGame();
-console.log(ThePsychicGameObj);
 
-ThePsychicGame.startGame();
+//------------------------------------------
 
+// class myClass {
+//     constructor() {
+//         this.arrLetters = []
+//     }
 
-// let geusss = 9;
-// let wins = 0;
-// let loses = 0;
-// let arrLetters = [];
-// let randomLetter = "s";
+//     startGame() {
+//         console.log(this.arrLetters, "myObj");
+//         return addEventListener("keypress", this.startstart)
+//     }
 
-
-// function randomLetterFunc() {
-//     let random = Math.floor(Math.random() * 26);
-//     let arrAlphabet = "abcdefghijklmnopqrstuvwxy".split("");
-//     return arrAlphabet[random];
+//     startstart(e) {
+//         this.arrLetters.push(e.key)
+//     }
 // }
 
-// function restartGame() {
-//     geusss = 9;
-//     guess.innerHTML = 9;
-//     arrLetters = [];
-//     randomLetter = randomLetterFunc();
-// }
+// const myObj = new myClass()
+
+// myObj.startGame()
+
+//------------------------------------------
 
 
-// function startGame() {
 
-//     addEventListener("keypress", e => {
-//         if (e.key.match(/[a-z]/i)) {
-//             if (geusss !== 0) {
-//                 if (e.key !== randomLetter) {
-//                     arrLetters.push(e.key)
-//                     geusss--;
-//                     guess.innerHTML = geusss;
-//                     letter.innerHTML = arrLetters.map((el) => {
-//                         return el;
-//                     }).join(", ");
+// class ThePsychicGame {
+//     // win = 0;
+//     // lose = 0;
+//     // guess = 9;
+//     // letter = 0;
+//     // randomLetter = "s";
+//     // arr = [];
+
+
+//     constructor() { //win, lose, guess, letter, arrLetters, randomLetter
+//         this.win = win;
+//         this.lose = lose;
+//         this.guess = guess;
+//         this.letter = letter;
+//         this.arrLetters = [];
+//         this.randomLetter = "randomLetter";
+//     }
+
+//     randomLetterFunc() {
+//         let random = Math.floor(Math.random() * 26);
+//         let arrAlphabet = "abcdefghijklmnopqrstuvwxy".split("");
+//         return arrAlphabet[random];
+//     };
+
+//     restartGame() {
+//         this.guess = 9;
+//         guessId.innerHTML = 9;
+//         this.arrLetters = [];
+//         this.randomLetter = this.randomLetterFunc();
+//     };
+
+
+//     startGame() {
+        
+//         console.log(this.arrLetters, "arr");
+//         addEventListener("keypress", function (e) {
+//             if (e.key.match(/[a-z]/i)) {
+//                 if (this.guess !== 0) {
+//                     if (e.key !== this.randomLetter) {
+//                         this.arrLetters.push(e.key);
+//                         this.guess--;
+//                         guessId.innerHTML = this.guess;
+//                         letterId.innerHTML = this.arrLetters.map((el) => {
+//                             return el;
+//                         }).join(", ");
+//                     } else {
+//                         this.win++;
+//                         winId.innerHTML = this.win;
+//                         letterIsId.innerHTML = this.randomLetter;
+//                         guessId.innerHTML = 9;
+//                         this.restartGame();
+//                     }
+//                     console.log(this.randomLetter);
 //                 } else {
-//                     wins++;
-//                     win.innerHTML = wins;
-//                     letterIs.innerHTML = randomLetter;
-//                     guess.innerHTML = 9;
-//                     restartGame();
+//                     this.lose++;
+//                     loseId.innerHTML = this.lose;
+//                     this.restartGame();
 //                 }
-//                 console.log(randomLetter);
-//             } else {
-//                 loses++;
-//                 lose.innerHTML = loses;
-//                 restartGame();
+
 //             }
+//         })
 
-//         }
-//     })
+//     };
 
-// };
+// }
 
-// startGame();
+// // const ThePsychicGameObj = new ThePsychicGame();
+// const ThePsychicGameObj = new ThePsychicGame(); //0, 0, 9, 0, [], "s"
+// ThePsychicGameObj.startGame()
+
+// ThePsychicGame.startGame();
+
+
+
+
